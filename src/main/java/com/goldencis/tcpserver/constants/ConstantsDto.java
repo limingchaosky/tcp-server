8a4caef0d162c9e5572aa69dd64b84e9f2d5abdc
limingchaosky <limingchaosky@163.com>
2018-10-23 08:31:27 +0800

    1.创建常量实体类。

---
+++ src/main/java/com/goldencis/tcpserver/constants/ConstantsDto.java
@@ -0,0 +1,17 @@
+package com.goldencis.tcpserver.constants;
+
+/**
+ * Created by limingchao on 2018/10/22.
+ */
+public class ConstantsDto {
+
+    public static final Integer TCP_PROTOCOL_VERIFY = 0x11ffeedd;
+
+    public static final Integer TCP_PROTOCOL_VERSION = 0x1000;
+
+    public static final String CONNECT_VNC_SERVER = "connectVncServer_c2s";
+
+    public static final String CONNECT_VNC_CLIENT = "connectVncClient_c2s";
+
+    public static final String CONNECT_VNC_SUCCESS = "connectVncSucc_s2c";
+}
